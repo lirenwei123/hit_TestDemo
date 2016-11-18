9e784edb17229043068fe91e94fabdd9e9c59ad1
Capital <303624150@qq.com>
2016-11-19 00:23:16 +0800

    Initial Commit

---
+++ hit_TestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hit_TestDemo
+//
+//  Created by rwli on 16/11/19.
+//  Copyright © 2016年 companyName. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
